596e25c85cd27a4575a17e8250243b0c68679407
Hakkı Sağdıç <hakkisagdic@gmail.com>
2020-11-14 11:46:47 +0300

    model binding explained

---
+++ edx-project/Models/DTO/CreateOrUpdateDTO.cs
@@ -0,0 +1,27 @@
+﻿namespace edx_project.Models.DTO
+{
+    public class CreateOrUpdateDTO
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public Genre[] Genres { get; set; }
+        public bool IsInStore { get; set; }
+        public Operation Operation { get; set; }
+    }
+
+    public enum Genre
+    {
+        Action,
+        Comedy,
+        War,
+        // ...
+    }
+
+    public enum Operation
+    {
+        Create,
+        Update,
+        // ...
+    }
+}
