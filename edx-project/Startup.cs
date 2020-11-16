754a5b2c4fe9889ddd57d75dd635cc5e53c745d1
Hakkı Sağdıç <hakkisagdic@gmail.com>
2020-11-16 08:43:49 +0300

    minor addings.

--- edx-project/Startup.cs
+++ edx-project/Startup.cs
@@ -19,7 +19,7 @@ namespace edx_project
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
             //services.AddControllersWithViews(configuration => configuration.EnableEndpointRouting=false);
         }
 
@@ -36,7 +36,7 @@ namespace edx_project
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
